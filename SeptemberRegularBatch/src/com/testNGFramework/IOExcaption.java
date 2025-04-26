874376ab49c8185693b8b2c21d8d000bb5cb9c4c
Manojkumar Ramu <154123389+manorjkumar@users.noreply.github.com>
2025-04-26 11:11:36 +0530

    Delete SeptemberRegularBatch directory

--- SeptemberRegularBatch/src/com/testNGFramework/IOExcaption.java
+++
@@ -1,5 +0,0 @@
-package com.testNGFramework;
-
-public class IOExcaption extends Exception {
-
-}
--- SeptemberRegularBatch/src/com/testNGFramework/TakeScrennshot.java
+++
@@ -1,7 +0,0 @@
-package com.testNGFramework;
-
-import java.io.File;
-
-public class TakeScrennshot extends File {
-
-}
